a6c26ca8afac77e7bfd41d421e28b2f8a4529c3c
jakekviti <thevitij64@gmail.com>
2021-11-12 16:19:10 -0500

    use files_get_contents to get info from API

--- search.php
+++ search.php
@@ -1,5 +1,9 @@
 <?php
-
-     print_r($_GET);
+    $searchTerm = $_GET['term'];
+    $keywordURL = "https://api.themoviedb.org/3/search/keyword?api_key=<<apikey>>&query=$searchTerm";
+    print_r($_GET['term']);
+    echo "<br>";
+    $test = file_get_contents($keywordURL);
+    echo $test;
 
 ?>
