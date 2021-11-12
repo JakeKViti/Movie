f5297df12cbd40217c18da76644016a17d8b2433
jakekviti <thevitij64@gmail.com>
2021-11-12 13:21:27 -0500

    Create Form on Index Page

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     <title>Movies</title>
 </head>
 <body>
-    
+<div class="indexPage">
+        <div class="mainSection">
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <input class="searchBox" type="text" name="term">
+                        <input class="searchButton" type="submit" value="Search">
+                    </form>
+                </div>
+        </div>
+    </div>
 </body>
 </html>
