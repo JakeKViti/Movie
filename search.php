40f863ec6c73f38343709d0cba2c86c1df0d088d
jakekviti <thevitij64@gmail.com>
2021-11-12 17:25:56 -0500

    Search term gets the keyword id and the keyword id makes a call to get the list of movies with that keyword

--- search.php
+++ search.php
@@ -1,9 +1,15 @@
 <?php
     $searchTerm = $_GET['term'];
-    $keywordURL = "https://api.themoviedb.org/3/search/keyword?api_key=<<apikey>>&query=$searchTerm";
+    $termLength = strlen($_GET['term']);
+    $keywordUrL = "https://api.themoviedb.org/3/search/keyword?api_key=<<apikey>>&query=$searchTerm";
     print_r($_GET['term']);
     echo "<br>";
-    $test = file_get_contents($keywordURL);
-    echo $test;
-
+    echo "$termLength";
+    echo "<br>";
+    $test = file_get_contents($keywordUrL, false, null, 30, 30);
+    $idNumber = filter_var($test, FILTER_SANITIZE_NUMBER_FLOAT);
+    echo $idNumber;
+    $movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=<<apikey>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_keywords=$idNumber";
+    $movieSearch = file_get_contents($movieUrl);
+    echo $movieSearch;
 ?>
