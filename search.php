767de47fc4cd67d5aecf2e2189eb610097a3d75b
jakekviti <thevitij64@gmail.com>
2021-11-12 21:00:19 -0500

    Got titles, genre_ids, and release dates from API

--- search.php
+++ search.php
@@ -1,15 +1,29 @@
 <?php
+    echo"<pre>";
     $searchTerm = $_GET['term'];
     $termLength = strlen($_GET['term']);
-    $keywordUrL = "https://api.themoviedb.org/3/search/keyword?api_key=<<apikey>>&query=$searchTerm";
+    $keywordUrL = "https://api.themoviedb.org/3/search/keyword?api_key=&query=$searchTerm";
     print_r($_GET['term']);
     echo "<br>";
     echo "$termLength";
     echo "<br>";
     $test = file_get_contents($keywordUrL, false, null, 30, 30);
     $idNumber = filter_var($test, FILTER_SANITIZE_NUMBER_FLOAT);
-    echo $idNumber;
-    $movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=<<apikey>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_keywords=$idNumber";
-    $movieSearch = file_get_contents($movieUrl);
-    echo $movieSearch;
+    $movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_keywords=$idNumber";
+    $movieSearch = file_get_contents($movieUrl, true);
+    $movieArray = array($movieSearch);
+
+
+    $json_data = json_decode($movieSearch, true);
+    //print_r($json_data['results'][0]);
+
+    for($i = 0; $i <= count($json_data['results']); $i++){
+        @print_r($json_data['results'][$i]['original_title']);
+        echo " ";
+        @print_r($json_data['results'][$i]['genre_ids']);
+        echo " ";
+        @print_r($json_data['results'][$i]['release_date']);
+        echo "<br>";
+    }
+    
 ?>
