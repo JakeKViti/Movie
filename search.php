245229a82b64ac0be795e66c4c083a0f3973244f
jakekviti <thevitij64@gmail.com>
2021-11-13 13:21:37 -0500

    Add Search Bar to search.php

--- search.php
+++ search.php
@@ -1,13 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Movies</title>
+</head>
+<body>
+<div class="indexPage">
+        <div class="mainSection">
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <input class="searchBox" type="text" name="term">
+                        <input class="searchButton" type="submit" value="Search">
+                    </form>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
+
 <?php
     echo"<pre>";
     $searchTerm = $_GET['term'];
-    $termLength = strlen($_GET['term']);
+    
     $keywordUrL = "https://api.themoviedb.org/3/search/keyword?api_key=&query=$searchTerm";
     print_r($_GET['term']);
     echo "<br>";
-    echo "$termLength";
-    echo "<br>";
-    $test = file_get_contents($keywordUrL, false, null, 30, 30);
+    @$test = file_get_contents($keywordUrL, false, null, 30, 30);
     $idNumber = filter_var($test, FILTER_SANITIZE_NUMBER_FLOAT);
     $movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_keywords=$idNumber";
     $movieSearch = file_get_contents($movieUrl, true);
@@ -17,11 +34,11 @@
     $json_data = json_decode($movieSearch, true);
     //print_r($json_data['results'][0]);
 
-    for($i = 0; $i <= count($json_data['results']); $i++){
+    for($i = 0; $i < count($json_data['results']); $i++){
         @print_r($json_data['results'][$i]['original_title']);
         echo " ";
         //@print_r($json_data['results'][$i]['genre_ids']);
-        foreach($json_data['results'][$i]['genre_ids'] as $genre){
+        foreach(@$json_data['results'][$i]['genre_ids'] as $genre){
             switch($genre){
                 case 12:
                     echo "Adventure ";
@@ -88,3 +105,4 @@
     }
     
 ?>
+
